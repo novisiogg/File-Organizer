e4a8673c17f299f115c79d107c53ef3659feaa9c
unknown <ahmedmakkass12@gmail.com>
2026-05-05 16:58:33 +0300

    fixed logger

--- logger.py
+++ logger.py
@@ -1,39 +1,31 @@
-from functools import wraps
 import logging
-from pathlib import Path
+import functools
 import time
+from pathlib import Path
 
-DECORATOR_DIR = Path(__file__).resolve().parent
+LOG_DIR = Path(__file__).resolve().parent / "logs"
+LOG_DIR.mkdir(exist_ok=True)
+LOG_FILE = LOG_DIR / "app.log"
 
-log_dir = DECORATOR_DIR / "logs"
-
-log_dir.mkdir(exist_ok=True)
-
-log_file_path = log_dir / "app.log"
-
-
-# logger configuration
 logging.basicConfig(
-    filename=log_file_path,
+    level=logging.INFO,
+    filename=str(LOG_FILE),
     filemode="a",
     format="%(asctime)s - %(name)s - %(levelname)s: %(message)s",
 )
 
-
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("File Organizer")
 
 
 def log(func):
-    @wraps(func)
+    @functools.wraps(func)
     def inner(*args, **kwargs):
-        time_before = time.perf_counter()
+        start = time.perf_counter()
         result = func(*args, **kwargs)
-        duration = time.perf_counter() - time_before
-
-        msg = f"{func.__name__} | Duration: {duration:.2f}s"
+        duration = time.perf_counter() - start
+        msg = f"{func.__name__} | Duration: {duration:.4f}s"
         if result is not None:
             msg += f" | Result: {result}"
-
         logger.info(msg)
         return result
 
