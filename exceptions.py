b5e5e80c0144d256bf00f7069437c76e7bfad177
unknown <ahmedmakkass12@gmail.com>
2026-05-05 16:49:49 +0300

    added custom exceptions

---
+++ exceptions.py
@@ -0,0 +1,13 @@
+class InvalidFolderError(Exception):
+    """Checks for non-existent folders."""
+    def __init__(self, message, foldername):
+        super().__init__(message)
+        self.message = message
+        self.foldername = foldername
+
+    def __str__(self):
+        return f"{self.message}: '{self.foldername}'"
+
+class ProtectedSystemFolder(InvalidFolderError):
+    """Checks for system-protected folders. (System32, Windows, etc..)"""
+    pass
