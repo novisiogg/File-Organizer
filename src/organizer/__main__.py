a21fde48ee3db2e5ad71640b96770dd7642aa634
unknown <ahmedmakkass12@gmail.com>
2026-05-06 14:17:57 +0300

    fixed layout for the help message

--- src/organizer/__main__.py
+++ src/organizer/__main__.py
@@ -244,11 +244,10 @@ def main():
         formatter_class=argparse.RawDescriptionHelpFormatter,
         epilog="""
 Examples:
-  organize C:\\Users\\name\\Downloads # organize the Downloads Folders
-  organize "C:\\Users\\name\\Downloads" --dry-run # preview the changes
-  organize .          # organize current directory
-  organize -u 5 # undo the last 5 moves 
-  organize 
+  organize C:\\Users\\name\\Downloads -> Organize Downloads folder.
+  organize "C:\\Users\\name\\Downloads" --dry-run -> See the queued changes. 
+  organize . -r -> Organize current directory along its subdirectories                       
+  organize C:\\Users\\name\\Downloads -u 5 -> Undo last 5 changes to the Downloads folder
 """,
     )
 
